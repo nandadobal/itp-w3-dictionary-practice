65b6b85f5beba781030d4866179b8a7476d59c44
nandadobal <nanda.dobal@gmail.com>
2017-12-03 18:05:41 +0000

    Update main.py

--- assignment_2/main.py
+++ assignment_2/main.py
@@ -1,2 +1,11 @@
 def calculate_purchase_price(purchase, set_to_dict=False):
-    pass
+    total = purchase['total']
+    for i in purchase['books']:
+        total = total + i['price']
+    if set_to_dict == True:
+        purchase['total'] = total
+        return total
+    else:
+        return total
+    
+
